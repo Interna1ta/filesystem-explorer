13798bc22167f2292ad0a7f124e4c9fcb8dd4c86
Jose1i1o <guadix88@gmail.com>
2021-11-02 16:26:51 +0100

    feat:sb-admin-2 install

---
+++ Filesystem/modules/login.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $nameDb = "Galileo";
+    $emailDb = "scaramouche@willyoudothefandango.com";
+    $passwordDb = "priscila";
+
+    $passwordHash = password_hash($passwordDb, PASSWORD_BCRYPT);
+
+    if(password_verify($password, $passwordHash) && $email == $emailDb && $name == $nameDb){
+        $_SESSION['name'] = $name;
+        header('Location: ../dashboard.php');
+    } else {
+        $_SESSION['errorMessage'] = "This email or password is not in our database";
+        header('Location: ../index.php');
+    }
+
+?>
